1d38146169d03bfc70f426e356fbdb5e01b6e600
AngelSoto03 <angel.soto@my.simpson.edu>
2020-08-27 15:42:40 -0500

    Work on Lab 1

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,2 +1,4 @@
 print("Hello, my name is Angel")
 print("I like playing sports with my friends and other people")
+print("Singing is one of my greatest passions in life")
+print("Lionel Messi is the best soccer player in soccer history")
